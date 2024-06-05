cb22a40ae2dd20f12f9b93dbd2a7df91cbfc2c9b
Johnny Chau <johnny.chau@live.com>
2024-06-05 03:19:33 -0700

    Create mac.sh

---
+++ mac.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Step 1: Update package lists
+echo "Updating package lists..."
+brew update || { echo "Failed to update package lists! Exiting..."; exit 1; }
+
+# Step 2: Install required packages
+echo "Installing required packages..."
+brew install curl wget git tmux nano unzip || { echo "Failed to install required packages! Exiting..."; exit 1; }
+
+# Step 3: Install Rust
+echo "Installing Rust..."
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y || { echo "Failed to install Rust! Exiting..."; exit 1; }
+echo 'source $HOME/.cargo/env' >> $HOME/.zshrc
+source $HOME/.zshrc
+
+# Step 4: Download and install Go
+echo "Installing Go..."
+wget https://go.dev/dl/go1.20.11.darwin-amd64.tar.gz || { echo "Failed to download Go! Exiting..."; exit 1; }
+sudo tar -C /usr/local -xzf go1.20.11.darwin-amd64.tar.gz || { echo "Failed to extract Go! Exiting..."; exit 1; }
+echo 'export PATH=$PATH:/usr/local/go/bin' >> $HOME/.zshrc
+source $HOME/.zshrc
+
+# Step 4.1: Configure the firewall
+echo "Configuring the firewall..."
+sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setglobalstate on || { echo "Failed to enable firewall! Exiting..."; exit 1; }
+sudo /usr/libexec/ApplicationFirewall/socketfilterfw --add /usr/bin/ssh || { echo "Failed to allow SSH! Exiting..."; exit 1; }
+sudo /usr/libexec/ApplicationFirewall/socketfilterfw --add /usr/local/go/bin || { echo "Failed to allow Go! Exiting..."; exit 1; }
+sudo /usr/libexec/ApplicationFirewall/socketfilterfw --add /usr/bin/git || { echo "Failed to allow Git! Exiting..."; exit 1; }
+sudo /usr/libexec/ApplicationFirewall/socketfilterfw --getglobalstate || { echo "Failed to get firewall status! Exiting..."; exit 1; }
+
+# Step 5: Clone ceremonyclient repository
+echo "Cloning ceremonyclient repository..."
+git clone https://source.quilibrium.com/quilibrium/ceremonyclient /ceremonyclient || { echo "Failed to clone repository! Exiting..."; exit 1; }
+cd /ceremonyclient/node/ || { echo "Failed to change directory! Exiting..."; exit 1; }
